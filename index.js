c329d3494068e94bac21ed5af2a2f98e9daf176f
Zhongming <atenliao@gmail.com>
2023-03-15 15:42:32 -0700

    complete index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()* 10) +1;
+
+const num3 = 15;
+const num4 = 11;
+const mod = num3 % num4;
+
+const max = Math.max(1,4,5,8,11,13,2,16,17,20)
